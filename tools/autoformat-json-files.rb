04b95f071799095f597a4ee0ee7fe9c40d79dc76
Ricky Mondello <rmondello@apple.com>
2020-10-25 15:50:11 -0700

    Teach tools/autoformat-json-files.rb to sort the JSON files, then run it

--- tools/autoformat-json-files.rb
+++ tools/autoformat-json-files.rb
@@ -11,27 +11,33 @@ Dir.chdir(quirks_dir)
 changed_any_or_error = false
 error_happened = false
 
-Dir.glob('*.json').each do |fPath|
-    relativePath = File.join("quirks", fPath)
+Dir.glob('*.json').each do |file_path|
+  relative_path = File.join("quirks", file_path)
 
-    begin
-        original = File.read(fPath)
-        parsed = JSON.parse(original)
-        reformatted = JSON.pretty_generate(parsed, indent: '    ') + "\n"
-        if original != reformatted
-            changed_any_or_error = true
-            File.write(fPath, reformatted)
-            puts("Reformatted '#{relativePath}'")
-        end
-    rescue => exception
-        changed_any_or_error = error_happened = true
-        STDERR.puts("Issue parsing & reformatting '#{relativePath}' - #{exception.to_s.split("\n")[0]}")
+  begin
+    original_file_contents = File.read(file_path)
+    contents_as_object = JSON.parse(original_file_contents)
+    if contents_as_object.is_a? Hash
+      contents_as_object = contents_as_object.sort_by { |key| key }.to_h
+    else
+      contents_as_object = contents_as_object.sort
     end
+
+    reformatted_file_contents = JSON.pretty_generate(contents_as_object, indent: '    ') + "\n"
+    if original_file_contents != reformatted_file_contents
+      changed_any_or_error = true
+      File.write file_path, reformatted_file_contents
+      puts("Reformatted '#{relative_path}'")
+    end
+  rescue => exception
+    changed_any_or_error = error_happened = true
+    STDERR.puts("Issue parsing & reformatting '#{relative_path}' - #{exception.to_s.split("\n")[0]}")
+  end
 end
 
 if !changed_any_or_error
-    puts('No files needed to be reformatted!')
+  puts "No files needed to be reformatted!"
 end
 if error_happened
-    exit(1)
+  exit 1
 end
