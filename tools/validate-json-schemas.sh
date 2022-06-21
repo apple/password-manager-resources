02fe22eea4664eb52565553b3c42b72ae175549d
Samar Sunkaria <sameesunkaria@me.com>
2022-06-21 17:59:54 +0200

    Add a script to validate quirks against their schemas

---
+++ tools/validate-json-schemas.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+if ! command -v ajv &> /dev/null
+then
+    echo "The 'ajv' command is required, please install the 'ajv-cli' package"
+    exit 1
+fi
+
+# Finds all JSON files in the quirks directory and validates them against the corresponding schema.
+find quirks -name '*.json' -print0 -maxdepth 1 | while IFS= read -r -d '' filename; do
+    schema="quirks/schemas/$(basename "$filename" .json)-schema.json"
+    echo "Validating $filename against $schema"
+    ajv -s "$schema" -d "$filename" --spec=draft2020
+done
